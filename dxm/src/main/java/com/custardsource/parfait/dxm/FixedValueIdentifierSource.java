c284b1c754b3806cfff8aee4a5993855b5df8fc8
funkwit <devnull@localhost>
2009-11-09 04:56:24 +0000

    Implementation of fixed-value identifier source

---
+++ dxm/src/main/java/com/custardsource/parfait/dxm/FixedValueIdentifierSource.java
@@ -0,0 +1,28 @@
+package com.custardsource.parfait.dxm;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+public class FixedValueIdentifierSource implements IdentifierSource {
+    private final Map<String, Integer> reservedIds;
+    private final IdentifierSource fallback;
+
+    public FixedValueIdentifierSource(Map<String, Integer> reservedIds, IdentifierSource fallback) {
+        this.reservedIds = reservedIds;
+        this.fallback = fallback;
+    }
+
+    @Override
+    public int calculateId(String name, Set<Integer> usedIds) {
+        Integer reservedId = reservedIds.get(name);
+        if (reservedId == null || usedIds.contains(reservedId)) {
+            return fallback.calculateId(name, Sets.union(usedIds, new HashSet<Integer>(reservedIds
+                    .values())));
+        }
+        return reservedId;
+    }
+
+}
---
+++ dxm/src/test/java/com/custardsource/parfait/dxm/FixedValueIdentifierSourceTest.java
@@ -0,0 +1,63 @@
+package com.custardsource.parfait.dxm;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+public class FixedValueIdentifierSourceTest {
+    private static final String PREDEFINED_STRING = "this.that.theother";
+    private static final int PREDEFINED_IDENTIFIER = 7;
+
+    private static final int FIXED_FALLBACK = 0xFEEDaBEE;
+    private static final IdentifierSource FALLBACK_SOURCE = new IdentifierSource() {
+        @Override
+        public int calculateId(String name, Set<Integer> usedIds) {
+            return FIXED_FALLBACK;
+        }
+    };
+
+    @Test
+    public void shouldFallBackWhenNoValueProvided() {
+        assertEquals(FIXED_FALLBACK, emptySource().calculateId(PREDEFINED_STRING,
+                noExistingIdentifiers()));
+    }
+
+    @Test
+    public void shouldReturnFixedValueWhenProvided() {
+        assertEquals(PREDEFINED_IDENTIFIER, populatedSource().calculateId(PREDEFINED_STRING,
+                noExistingIdentifiers()));
+    }
+
+    @Test
+    public void shouldReturnFallBackWhenValueCollides() {
+        assertEquals(FIXED_FALLBACK, populatedSource().calculateId(PREDEFINED_STRING,
+                collidingIdentifiers()));
+    }
+
+    private IdentifierSource emptySource() {
+        return buildSource(Maps.<String, Integer> newHashMap());
+    }
+
+    private IdentifierSource populatedSource() {
+        return buildSource(ImmutableMap.of(PREDEFINED_STRING, PREDEFINED_IDENTIFIER));
+    }
+
+    private IdentifierSource buildSource(Map<String, Integer> mappings) {
+        return new FixedValueIdentifierSource(mappings, FALLBACK_SOURCE);
+    }
+
+    private Set<Integer> noExistingIdentifiers() {
+        return Collections.emptySet();
+    }
+
+    private Set<Integer> collidingIdentifiers() {
+        return Collections.singleton(PREDEFINED_IDENTIFIER);
+    }
+}
