386cf1d483b1a4f3ff9722550ceb9700a0024890
funkwit <devnull@localhost>
2009-11-13 04:33:03 +0000

    Better error message for duplicate metrics

--- dxm/src/main/java/com/custardsource/parfait/dxm/PcpMetricInfo.java
+++ dxm/src/main/java/com/custardsource/parfait/dxm/PcpMetricInfo.java
@@ -1,81 +1,78 @@
-/**
- * 
- */
 package com.custardsource.parfait.dxm;
 
 import com.custardsource.parfait.dxm.types.TypeHandler;
 
 final class PcpMetricInfo implements PcpId, PcpOffset {
-        private final String metricName;
-        private final int id;
-        
-        private InstanceDomain domain;
-        private TypeHandler<?> typeHandler;
-        private int offset;
-        private PcpString shortHelpText;
-        private PcpString longHelpText;
-        
+    private final String metricName;
+    private final int id;
 
-        PcpMetricInfo(String metricName, int id) {
-            this.metricName = metricName;
-            this.id = id;
-        }
+    private InstanceDomain domain;
+    private TypeHandler<?> typeHandler;
+    private int offset;
+    private PcpString shortHelpText;
+    private PcpString longHelpText;
 
-        public int getId() {
-            return id;
-        }
-        
-        @Override
-        public int getOffset() {
-            return offset;
-        }
+    PcpMetricInfo(String metricName, int id) {
+        this.metricName = metricName;
+        this.id = id;
+    }
 
-        @Override
-        public void setOffset(int offset) {
-            this.offset = offset;
-        }
-        
-        String getMetricName() {
-            return metricName;
-        }
+    public int getId() {
+        return id;
+    }
 
-        TypeHandler<?> getTypeHandler() {
-            return typeHandler;
-        }
-        
-        void setTypeHandler(TypeHandler<?> typeHandler) {
-            if (this.typeHandler == null || this.typeHandler.equals(typeHandler)) {
-                this.typeHandler = typeHandler;
-            } else {
-                throw new IllegalArgumentException(
-                        "Two different type handlers cannot be registered for metric " + metricName);
-            }
-            
+    @Override
+    public int getOffset() {
+        return offset;
+    }
+
+    @Override
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    String getMetricName() {
+        return metricName;
+    }
+
+    TypeHandler<?> getTypeHandler() {
+        return typeHandler;
+    }
+
+    void setTypeHandler(TypeHandler<?> typeHandler) {
+        if (this.typeHandler == null || this.typeHandler.equals(typeHandler)) {
+            this.typeHandler = typeHandler;
+        } else {
+            throw new IllegalArgumentException(
+                    "Two different type handlers cannot be registered for metric " + metricName);
         }
 
-        InstanceDomain getInstanceDomain() {
-            return domain;
-        }
+    }
 
-        void setInstanceDomain(InstanceDomain domain) {
-            if (this.domain == null || this.domain.equals(domain)) {
-                this.domain = domain;
-            } else {
-                throw new IllegalArgumentException(
-                        "Two different instance domains cannot be set for metric " + metricName);
-            }
-        }
+    InstanceDomain getInstanceDomain() {
+        return domain;
+    }
 
-        PcpString getShortHelpText() {
-            return shortHelpText;
+    void setInstanceDomain(InstanceDomain domain) {
+        if (this.domain == null || this.domain.equals(domain)) {
+            this.domain = domain;
+        } else {
+            throw new IllegalArgumentException(
+                    "Two different instance domains cannot be set for metric " + metricName
+                            + " (old=" + this.domain + ", new=" + domain + ")");
         }
-        
-        PcpString getLongHelpText() {
-            return longHelpText;
-        }
+    }
 
-        void setHelpText(PcpString shortHelpText, PcpString longHelpText) {
-            this.shortHelpText = shortHelpText;
-            this.longHelpText = longHelpText;
-        }
+    PcpString getShortHelpText() {
+        return shortHelpText;
+    }
+
+    PcpString getLongHelpText() {
+        return longHelpText;
+    }
+
+    void setHelpText(PcpString shortHelpText, PcpString longHelpText) {
+        this.shortHelpText = shortHelpText;
+        this.longHelpText = longHelpText;
+    }
 }
