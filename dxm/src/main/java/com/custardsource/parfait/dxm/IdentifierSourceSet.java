ed0065bdfe5d8e036c79fdc02dae8fe60ed4d2fa
funkwit <devnull@localhost>
2010-01-21 04:32:17 +0000

    Add an explicit metric set for the most typical use case (caring about metric + indom identifiers, but not instance ones)

--- dxm/src/main/java/com/custardsource/parfait/dxm/IdentifierSourceSet.java
+++ dxm/src/main/java/com/custardsource/parfait/dxm/IdentifierSourceSet.java
@@ -5,7 +5,6 @@ public interface IdentifierSourceSet {
     public IdentifierSource instanceSource(String domain);
     public IdentifierSource metricSource();
     
-    
     public static IdentifierSourceSet DEFAULT_SET = new IdentifierSourceSet() {
         private final IdentifierSource DEFAULT_SOURCE = new HashingIdentifierSource();
         
@@ -22,6 +21,30 @@ public interface IdentifierSourceSet {
         @Override
         public IdentifierSource instanceDomainSource() {
             return DEFAULT_SOURCE;
+        }
+    };
+    
+    /**
+     * {@link IdentifierSourceSet} which will fail for indoms and metrics, but allow default hashed
+     * values for Instances
+     */
+    public static IdentifierSourceSet EXPLICIT_SET = new IdentifierSourceSet() {
+        private final IdentifierSource DEFAULT_SOURCE = new HashingIdentifierSource();
+        private final IdentifierSource ERROR_SOURCE = new ErrorThrowingIdentifierSource();
+        
+        @Override
+        public IdentifierSource metricSource() {
+            return ERROR_SOURCE;
+        }
+        
+        @Override
+        public IdentifierSource instanceSource(String domain) {
+            return DEFAULT_SOURCE;
+        }
+        
+        @Override
+        public IdentifierSource instanceDomainSource() {
+            return ERROR_SOURCE;
         }
     };
 }
