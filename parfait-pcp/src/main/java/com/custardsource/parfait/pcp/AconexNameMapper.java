8a69e7ea2d4796387ebd5b3d650878dbafda40d2
funkwit <devnull@localhost>
2010-01-28 00:30:48 +0000

    New NameMapper tweaks for session bins, spaceused/ keypool

--- parfait-pcp/src/main/java/com/custardsource/parfait/pcp/AconexNameMapper.java
+++ parfait-pcp/src/main/java/com/custardsource/parfait/pcp/AconexNameMapper.java
@@ -25,7 +25,13 @@ public class AconexNameMapper extends RegexSequenceNameMapper {
 
     private static final Pattern SESSION_PATTERN = Pattern
             .compile("aconex.sessions.([^\\.]+)\\.(.*)");
-    private static final String SESSION_REPLACEMENT = "aconex.sessions[%P$1].$2";
+    private static final String SESSION_REPLACEMENT = "aconex.sessions.bin[%P$1].$2";
+
+    private static final Pattern SPACEUSED_PATTERN = Pattern.compile("aconex.spaceused.([^\\.]+)\\.(.*)");
+    private static final String SPACEUSED_REPLACEMENT = "aconex.spaceused[%P$1].$2";
+
+    private static final Pattern KEYPOOL_PATTERN = Pattern.compile("aconex\\.keypool\\.(.*)");
+    private static final String KEYPOOL_REPLACEMENT = "aconex.keypool[%P$1]";
 
     private static final Pattern GENERIC_PATTERN = Pattern.compile("aconex.([^\\[]+)");
     private static final String GENERIC_REPLACEMENT = "aconex[%I].$1";
@@ -41,13 +47,17 @@ public class AconexNameMapper extends RegexSequenceNameMapper {
                 EMAIL_SENT_REPLACEMENT, instance));
         Replacement cacheReplacement = new Replacement(CACHE_PATTERN, replacementPatternFor(
                 CACHE_REPLACEMENT, instance));
-        Replacement sessionReplacement = new Replacement(SESSION_PATTERN, replacementPatternFor(
+        Replacement sessionReplacement = new Replacement(SPACEUSED_PATTERN, replacementPatternFor(
+                SPACEUSED_REPLACEMENT, instance));
+        Replacement spaceUsedReplacement = new Replacement(SESSION_PATTERN, replacementPatternFor(
                 SESSION_REPLACEMENT, instance));
+        Replacement keypoolReplacement = new Replacement(KEYPOOL_PATTERN, replacementPatternFor(
+                KEYPOOL_REPLACEMENT, instance));
         Replacement genericReplacement = new Replacement(GENERIC_PATTERN, replacementPatternFor(
                 GENERIC_REPLACEMENT, instance));
 
         return ImmutableList.of(controllerReplacement, emailReplacement, cacheReplacement,
-                sessionReplacement, genericReplacement);
+                spaceUsedReplacement, sessionReplacement, keypoolReplacement, genericReplacement);
     }
 
     private static String replacementPatternFor(String rawReplacement, String instance) {
