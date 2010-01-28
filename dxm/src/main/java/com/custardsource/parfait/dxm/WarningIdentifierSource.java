6086211c3e5c112eaefeff66a64edb96a4574029
funkwit <devnull@localhost>
2010-01-28 00:31:16 +0000

    Add WarningIdentifierSource

---
+++ dxm/src/main/java/com/custardsource/parfait/dxm/WarningIdentifierSource.java
@@ -0,0 +1,27 @@
+package com.custardsource.parfait.dxm;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.log4j.Logger;
+
+/**
+ * {@link IdentifierSource} which logs a warning on every generation, and returns a dummy value.
+ * Useful for generating a list of IDs which should later be explicity-provided.
+ */
+public class WarningIdentifierSource implements IdentifierSource {
+    private static final Logger LOG = Logger.getLogger(WarningIdentifierSource.class);
+
+    private final AtomicInteger nextId;
+
+    public WarningIdentifierSource(int initialId) {
+        nextId = new AtomicInteger(initialId);
+    }
+
+    @Override
+    public int calculateId(String name, Set<Integer> usedIds) {
+        LOG.warn("No identifier provided for value " + name + "; please specify an explicit ID");
+        return nextId.getAndIncrement();
+    }
+
+}
