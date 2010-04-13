a714e1ab361d5dfb402a48551de71e4c6985a398
scott.nato <devnull@localhost>
2010-04-13 09:51:51 +0000

    Additional PcpMmvWriter constructor for simplified initialisation.
    Adds a constructor which can figure out a good MMV file name and
    location based on a simple, logical application name rather than
    a File.  This hides platform-specific MMV implementation details
    from the instrumented application code.
    
    Reviewed-by: Cowan and TallPaul.

---
+++ dxm/src/main/java/com/custardsource/parfait/dxm/PcpConfig.java
@@ -0,0 +1,48 @@
+package com.custardsource.parfait.dxm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+
+public class PcpConfig {
+
+    private final static String pcpConf = System.getenv("PCP_CONF");
+    private final static String pcpDir = System.getenv("PCP_DIR");
+
+    public File getRoot() {
+        return new File(pcpDir, "/");
+    }
+
+    private File getConfigFile() {
+        if (pcpConf == null) {
+            File etcDir = new File(getRoot(), "etc");
+            return new File(etcDir, "pcp.conf");
+        }
+        return new File(pcpConf);
+    }
+
+    public String getValue(String key) {
+        Properties properties = new Properties();
+        File configuration = getConfigFile();
+        InputStream is = null;
+        String value = null;
+ 
+        try {
+            is = new FileInputStream(configuration);
+            properties.load(is);
+            value = properties.get(key).toString();
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            // just drop these, and go with defaults
+        }
+        finally {
+            IOUtils.closeQuietly(is);
+        }
+        return value;
+    }
+}
