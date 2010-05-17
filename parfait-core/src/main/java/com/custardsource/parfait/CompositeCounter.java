d910f8bafff4e2c7f9e6ca72ff18ba9a591c36ca
pcowan <pcowan@cowan.engr.acx>
2010-05-17 17:18:16 +1000

    Used ImmutableList instead of ArrayList for clarity

--- parfait-core/src/main/java/com/custardsource/parfait/CompositeCounter.java
+++ parfait-core/src/main/java/com/custardsource/parfait/CompositeCounter.java
@@ -2,13 +2,13 @@ package com.custardsource.parfait;
 
 import java.util.Collection;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 
 public class CompositeCounter implements Counter {
     private final Collection<Counter> counters;
 
     public CompositeCounter(Collection<? extends Counter> counters) {
-        this.counters = Lists.newArrayList(counters);
+        this.counters = ImmutableList.copyOf(counters);
     }
 
     @Override
