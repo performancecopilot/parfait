d0104966e3072b3eb79ae8aa870f0a4cbcb95536
pcowan <pcowan@cowan.engr.acx>
2010-05-17 17:17:44 +1000

    Added test for CompositeCounter; changed to not require specifically Collection<Counter>

--- parfait-core/src/main/java/com/custardsource/parfait/CompositeCounter.java
+++ parfait-core/src/main/java/com/custardsource/parfait/CompositeCounter.java
@@ -6,21 +6,21 @@ import com.google.common.collect.Lists;
 
 public class CompositeCounter implements Counter {
     private final Collection<Counter> counters;
-    
-    public CompositeCounter(Collection<Counter> counters) {
+
+    public CompositeCounter(Collection<? extends Counter> counters) {
         this.counters = Lists.newArrayList(counters);
     }
 
     @Override
     public void inc() {
-        for(Counter counter : counters) {
+        for (Counter counter : counters) {
             counter.inc();
         }
     }
 
     @Override
     public void inc(long increment) {
-        for(Counter counter : counters) {
+        for (Counter counter : counters) {
             counter.inc(increment);
         }
     }
