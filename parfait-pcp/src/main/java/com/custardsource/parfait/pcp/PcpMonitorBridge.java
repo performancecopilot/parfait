9cc99a2ff94cea4cfa9d8b08b490e54a0bcba60e
Paul Cowan <paul@custardsource.com>
2010-05-19 12:00:12 +1000

    Changed to use live updates when writing metrics, instead of queueing for later use.

--- parfait-pcp/src/main/java/com/custardsource/parfait/pcp/PcpMonitorBridge.java
+++ parfait-pcp/src/main/java/com/custardsource/parfait/pcp/PcpMonitorBridge.java
@@ -1,14 +1,9 @@
 package com.custardsource.parfait.pcp;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
-
-import org.apache.log4j.Logger;
-import org.springframework.jmx.export.annotation.ManagedAttribute;
-import org.springframework.jmx.export.annotation.ManagedResource;
 
 import com.custardsource.parfait.Monitor;
 import com.custardsource.parfait.Monitorable;
@@ -20,6 +15,9 @@ import com.custardsource.parfait.dxm.PcpWriter;
 import com.custardsource.parfait.dxm.semantics.Semantics;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
+import org.apache.log4j.Logger;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 /**
  * PcpMonitorBridge bridges between the set of {@link Monitorable}s in the current system and a PCP
@@ -30,7 +28,7 @@ import com.google.common.collect.ImmutableMap;
 public class PcpMonitorBridge extends MonitoringView {
 
     private static final Logger LOG = Logger.getLogger(PcpMonitorBridge.class);
-    
+
     private static final TextSource DEFAULT_SHORT_TEXT_SOURCE = new MetricDescriptionTextSource();
     private static final TextSource DEFAULT_LONG_TEXT_SOURCE = new EmptyTextSource();
 
@@ -41,10 +39,9 @@ public class PcpMonitorBridge extends MonitoringView {
             Semantics.INSTANT, ValueSemantics.MONOTONICALLY_INCREASING, Semantics.COUNTER);
 
     private final ArrayBlockingQueue<Monitorable<?>> monitorablesPendingUpdate = new ArrayBlockingQueue<Monitorable<?>>(
-    		UPDATE_QUEUE_SIZE);
+            UPDATE_QUEUE_SIZE);
 
     private final Monitor monitor = new PcpMonitorBridgeMonitor();
-    private final Thread updateThread;
     private final MetricNameMapper mapper;
     private final TextSource shortTextSource;
     private final TextSource longTextSource;
@@ -55,29 +52,25 @@ public class PcpMonitorBridge extends MonitoringView {
      */
     private boolean outputValuesToPCPFile = true;
 
-	private volatile PcpWriter pcpWriter;
-
+    private volatile PcpWriter pcpWriter;
 
 
     public PcpMonitorBridge(PcpWriter writer) {
-    	this(writer, MonitorableRegistry.DEFAULT_REGISTRY);
+        this(writer, MonitorableRegistry.DEFAULT_REGISTRY);
     }
 
     public PcpMonitorBridge(PcpWriter writer, MonitorableRegistry registry) {
         this(writer, registry, MetricNameMapper.PASSTHROUGH_MAPPER, DEFAULT_SHORT_TEXT_SOURCE,
                 DEFAULT_LONG_TEXT_SOURCE);
     }
-    
+
     public PcpMonitorBridge(PcpWriter writer, MonitorableRegistry registry,
             MetricNameMapper mapper, TextSource shortTextSource, TextSource longTextSource) {
-		super(registry);
-		this.pcpWriter = Preconditions.checkNotNull(writer);
-		this.mapper = Preconditions.checkNotNull(mapper);
+        super(registry);
+        this.pcpWriter = Preconditions.checkNotNull(writer);
+        this.mapper = Preconditions.checkNotNull(mapper);
         this.shortTextSource = Preconditions.checkNotNull(shortTextSource);
         this.longTextSource = Preconditions.checkNotNull(longTextSource);
-		this.updateThread = new Thread(new Updater());
-		this.updateThread.setName("PcpMonitorBridge-Updater");
-		this.updateThread.setDaemon(true);
     }
 
     @Override
@@ -96,8 +89,8 @@ public class PcpMonitorBridge extends MonitoringView {
     protected void startMonitoring(Collection<Monitorable<?>> monitorables) {
         try {
             for (Monitorable<?> monitorable : monitorables) {
-            	monitorable.attachMonitor(monitor);
-            	MetricName metricName = mapper.map(monitorable.getName());
+                monitorable.attachMonitor(monitor);
+                MetricName metricName = mapper.map(monitorable.getName());
                 pcpWriter.addMetric(metricName,
                         convertToPcpSemantics(monitorable.getSemantics()), monitorable.getUnit(),
                         monitorable.get());
@@ -105,8 +98,6 @@ public class PcpMonitorBridge extends MonitoringView {
                         monitorable, metricName), longTextSource.getText(monitorable, metricName));
             }
             pcpWriter.start();
-
-            updateThread.start();
 
             LOG.info("PCP monitoring bridge started for writer [" + pcpWriter + "]");
         } catch (IOException e) {
@@ -118,47 +109,6 @@ public class PcpMonitorBridge extends MonitoringView {
         return SEMANTICS_MAP.get(semantics);
     }
 
-    /**
-     * The Updater is responsible for taking any Monitorables that are pending in the update queue
-     * and saving their current value to the PCP shared data file.
-     */
-    private class Updater implements Runnable {
-
-        public void run() {
-            try {
-                Collection<Monitorable<?>> monitorablesToUpdate = new ArrayList<Monitorable<?>>();
-                PcpWriter writerCopy;
-                while ((writerCopy = pcpWriter) != null) {
-                    try {
-                        monitorablesToUpdate.add(monitorablesPendingUpdate.take());
-                        monitorablesPendingUpdate.drainTo(monitorablesToUpdate);
-                        for (Monitorable<?> monitorable : monitorablesToUpdate) {
-							writerCopy.updateMetric(mapper.map(monitorable
-									.getName()), monitorable.get());
-                        }
-                        if (monitorablesPendingUpdate.size() >= UPDATE_QUEUE_SIZE) {
-                            LOG.warn("Update queue was full - some updates may have been lost.");
-                        }
-                        monitorablesToUpdate.clear();
-                    } catch (InterruptedException e) {
-                        LOG.error("Updater was unexpectedly interrupted", e);
-                    }
-                }
-            } catch (RuntimeException e) {
-                LOG.fatal("Updater dying because of unexpected exception", e);
-                throw e;
-            } catch (Error e) {
-                LOG.fatal("Updater dying because of unexpected exception", e);
-                throw e;
-            }
-        }
-    }
-
-    /**
-     * Responsible for adding any Monitorables that change to the queue of Monitorables that are
-     * pending update. This class will never block, if the update queue is ever full then the we
-     * just do nothing.
-     */
     private class PcpMonitorBridgeMonitor implements Monitor {
 
         public void valueChanged(Monitorable<?> monitorable) {
@@ -171,12 +121,16 @@ public class PcpMonitorBridge extends MonitoringView {
                 return;
             }
 
-            if (!monitorablesPendingUpdate.offer(monitorable)) {
-            	// Don't care about return value here. If this failed, the queue must be full;
-            	// This will get detected by the Updater and logged. We should do nothing here as we 
-            	// don't want to block.
-            }
+            writeUpdate(monitorable);
         }
+    }
+
+    private void writeUpdate(Monitorable<?> monitorable) {
+        PcpWriter writerCopy = pcpWriter;
+        if (writerCopy == null) {
+            return;
+        }
+        writerCopy.updateMetric(mapper.map(monitorable.getName()), monitorable.get());
     }
 
     @ManagedAttribute(description = "If set, value changes are written to an external file monitored PCP Agent.")
