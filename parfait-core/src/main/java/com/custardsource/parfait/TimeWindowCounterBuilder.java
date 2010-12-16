72659b43cd27cfe6a4c16b1d1867bf5dff8ea107
Paul Cowan <paul@custardsource.com>
2010-12-16 18:27:40 +1100

    Javadoc TimeWindowCounterBuilder

--- parfait-core/src/main/java/com/custardsource/parfait/TimeWindowCounterBuilder.java
+++ parfait-core/src/main/java/com/custardsource/parfait/TimeWindowCounterBuilder.java
@@ -10,6 +10,14 @@ import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
+/**
+ * Factory class to produce multiple sets of {@link TimeWindowCounter
+ * TimeWindowCounters} with a specific and consistent set of {@link TimeWindow
+ * TimeWindows}, and in turn produce {@link PollingMonitoredValue
+ * PollingMonitoredValues} which watch those TimeWindowCounters. Can either
+ * create the TimeWindowCounters from scratch, or 'copy' an existing
+ * MonitoredCounter.
+ */
 @ThreadSafe
 public class TimeWindowCounterBuilder {
 	private final List<TimeWindow> timeWindows;
@@ -28,6 +36,22 @@ public class TimeWindowCounterBuilder {
 		this.timeWindows = ImmutableList.copyOf(windows);
 	}
 
+	/**
+	 * Builds a new {@link CompositeCounter}, comprised of TimeWindowCounters,
+	 * and registers {@link PollingMonitoredValue}s to detect changes in their
+	 * values.
+	 * 
+	 * @param baseName
+	 *            the base name of the new MonitoredValues, which will have the
+	 *            window name, e.g. ".60s", appended to it for each window
+	 * @param baseDescription
+	 *            the base description of the new MonitoredValues, to be
+	 *            appended with e.g. " [60s]"
+	 * @param unit
+	 *            the {@link Unit} to use for the new values
+	 * @return a CompositeCounter wrapping a set of new TimeWindow-based
+	 *         counters
+	 */
 	public CompositeCounter build(String baseName, String baseDescription,
 			Unit<?> unit) {
 		List<Counter> counters = getSubCounters(baseName, baseDescription, unit);
@@ -56,12 +80,36 @@ public class TimeWindowCounterBuilder {
 		return counters;
 	}
 
+	/**
+	 * Creates a new CompositeCounter wrapping TimeWindowCounters (and creating
+	 * PollingMonitoredValues), using the supplied Monitorable's name,
+	 * description, and unit as the template.
+	 * 
+	 * @param templateMonitorable
+	 *            a Monitorable whose name, description, and unit should be
+	 *            copied
+	 * @return a CompositeCounter wrapping a set of new TimeWindow-based
+	 *         counters
+	 */
 	public CompositeCounter copyFrom(Monitorable<?> templateMonitorable) {
 		return build(templateMonitorable.getName(),
 				templateMonitorable.getDescription(),
 				templateMonitorable.getUnit());
 	}
 
+	/**
+	 * Creates a new CompositeCounter wrapping TimeWindowCounters (and creating
+	 * PollingMonitoredValues), using the supplied MonitoredCounter's name,
+	 * description, and unit as the template. <em>Also</em> wraps the supplied
+	 * MonitoredCounter itself (hence providing a single incrementable Counter
+	 * which will increment both an overall total and a set of TimeWindow
+	 * counters)
+	 * 
+	 * @param templateCounter
+	 *            a Counter whose name, description, and unit should be copied
+	 * @return a CompositeCounter wrapping a set of new TimeWindow-based
+	 *         counters and the supplied templateCounter
+	 */
 	public CompositeCounter wrapCounter(MonitoredCounter templateCounter) {
 		List<Counter> subCounters = Lists
 				.<Counter> newArrayList(templateCounter);
