75c3cefb4afc1de863f388732a323b73a7377afd
Paul Cowan <paul@custardsource.com>
2010-12-20 23:15:28 +1100

    New StepMeasurementSink which only passes on a certain fraction of events

---
+++ parfait-core/src/test/java/com/custardsource/parfait/timing/SamplingMeasurementSinkTest.java
@@ -0,0 +1,55 @@
+package com.custardsource.parfait.timing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SamplingMeasurementSinkTest {
+    @Test
+    public void zeroFractionShouldNeverSampleEvents() {
+        CountingMeasurementSink counter = new CountingMeasurementSink();
+        StepMeasurementSink sink = new SamplingMeasurementSink(counter, 0.0f);
+        assertEventSinkCounts(sink, 0, counter, 0, 0, 0, 0, 0);
+    }
+
+    @Test
+    public void oneFractionShouldSampleAllEventCounts() {
+        CountingMeasurementSink counter = new CountingMeasurementSink();
+        StepMeasurementSink sink = new SamplingMeasurementSink(counter, 1.0f);
+        assertEventSinkCounts(sink, 0, counter, 1, 2, 3, 4, 5);
+    }
+
+    @Test
+    public void eventsSampledShouldNeverExceedSpecifiedFraction() {
+        CountingMeasurementSink counter = new CountingMeasurementSink();
+        StepMeasurementSink sink = new SamplingMeasurementSink(counter, 0.33333f);
+        assertEventSinkCounts(sink, 0, counter, 1, 1, 1, 2, 2, 2, 3, 3);
+    }
+
+    @Test
+    public void nestedEventsShouldNotBeDelegated() {
+        CountingMeasurementSink counter = new CountingMeasurementSink();
+        StepMeasurementSink sink = new SamplingMeasurementSink(counter, 1.0f);
+        assertEventSinkCounts(sink, 1, counter, 0, 0, 0);
+    }
+
+    private void assertEventSinkCounts(StepMeasurementSink sink, int eventLevel,
+            CountingMeasurementSink counter, int... progressiveCounts) {
+        int seen = 0;
+        for (int progressiveCount : progressiveCounts) {
+            sink.handle(null, eventLevel);
+            seen++;
+            assertEquals(
+                    String.format("After %s events, %s should be sunk", seen, progressiveCount),
+                    progressiveCount, counter.eventsSunk);
+        }
+    }
+
+    private static class CountingMeasurementSink implements StepMeasurementSink {
+        private int eventsSunk = 0;
+
+        @Override
+        public void handle(StepMeasurements measurements, int level) {
+            eventsSunk++;
+        }
+    }
+}
