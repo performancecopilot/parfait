869ea9566ee023dcdd2deab15c9dccf1d7ae8386
psmith <psmith@aconex.com>
2012-02-18 13:52:06 +1100

    Introduce helper methods for calculating standard metrics across tests.

---
+++ parfait-benchmark/src/main/java/com/custardsource/parfait/benchmark/BlockedMetricHelper.java
@@ -0,0 +1,24 @@
+package com.custardsource.parfait.benchmark;
+
+import java.util.List;
+
+public class BlockedMetricHelper {
+    private BlockedMetricHelper() {
+    }
+
+    static long computeTotalBlockedCount(List<BlockedMetricCollector> blockedMetricCollectors) {
+        long totalBlockedCount = 0;
+        for (BlockedMetricCollector collector : blockedMetricCollectors) {
+            totalBlockedCount += collector.getTotalBlockedCount();
+        }
+        return totalBlockedCount;
+    }
+
+    static long computeTotalBlockedTime(List<BlockedMetricCollector> blockedMetricCollectors) {
+        long totalBlockedTime = 0;
+        for (BlockedMetricCollector collector : blockedMetricCollectors) {
+            totalBlockedTime += collector.getTotalBlockedTime();
+        }
+        return totalBlockedTime;
+    }
+}
