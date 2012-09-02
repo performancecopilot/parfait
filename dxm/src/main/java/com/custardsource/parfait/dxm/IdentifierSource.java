5a2b3c53e930405f5c46600202beb74f5171a42c
Yoram Kulbak <yoram.kulbak@gmail.com>
2012-09-02 21:59:09 +1000

    Make IdentifierSource public so that it can be implemented by users

--- dxm/src/main/java/com/custardsource/parfait/dxm/IdentifierSource.java
+++ dxm/src/main/java/com/custardsource/parfait/dxm/IdentifierSource.java
@@ -2,6 +2,6 @@ package com.custardsource.parfait.dxm;
 
 import java.util.Set;
 
-interface IdentifierSource {
+public interface IdentifierSource {
     int calculateId(String name, Set<Integer> usedIds);
 }
