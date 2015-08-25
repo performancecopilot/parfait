3ba93269da9cc16da6f27f2a47af0fc5fccfb85a
timols <tim.olshansky@gmail.com>
2015-08-24 18:05:57 -0700

    Plug the monitoring view into the standard dropwizard reporter lifecycle

--- parfait-dropwizard/src/main/java/com/custardsource/parfait/dropwizard/ParfaitReporterFactory.java
+++ parfait-dropwizard/src/main/java/com/custardsource/parfait/dropwizard/ParfaitReporterFactory.java
@@ -2,7 +2,12 @@ package com.custardsource.parfait.dropwizard;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ScheduledReporter;
+import com.custardsource.parfait.DynamicMonitoringView;
 import com.custardsource.parfait.MonitorableRegistry;
+import com.custardsource.parfait.MonitoringView;
+import com.custardsource.parfait.dxm.IdentifierSourceSet;
+import com.custardsource.parfait.dxm.PcpMmvWriter;
+import com.custardsource.parfait.pcp.PcpMonitorBridge;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.dropwizard.metrics.BaseReporterFactory;
@@ -17,9 +22,17 @@ public class ParfaitReporterFactory extends BaseReporterFactory {
     @NotNull
     private String prefix = "";
 
+    @NotNull
+    private String pcpName;
+
     private String registryName = "dropwizard-default";
 
     private Map<String, String> replacements = new LinkedHashMap<>();
+
+    private long quietPeriod = DynamicMonitoringView.defaultQuietPeriod();
+
+    @NotNull
+    private Integer clusterIdentifier;
 
     @JsonProperty
     public String getPrefix() {
@@ -29,6 +42,16 @@ public class ParfaitReporterFactory extends BaseReporterFactory {
     @JsonProperty
     public void setPrefix(String prefix) {
         this.prefix = prefix;
+    }
+
+    @JsonProperty
+    public String getPcpName() {
+        return pcpName;
+    }
+
+    @JsonProperty
+    public void setPcpName(String pcpName) {
+        this.pcpName = pcpName;
     }
 
     @JsonProperty
@@ -51,14 +74,42 @@ public class ParfaitReporterFactory extends BaseReporterFactory {
         this.replacements = replacements;
     }
 
+    @JsonProperty
+    public long getQuietPeriod() {
+        return quietPeriod;
+    }
+
+    @JsonProperty
+    public void setQuietPeriod(long quietPeriod) {
+        this.quietPeriod = quietPeriod;
+    }
+
+    public Integer getClusterIdentifier() {
+        return clusterIdentifier;
+    }
+
+    public void setClusterIdentifier(Integer clusterIdentifier) {
+        this.clusterIdentifier = clusterIdentifier;
+    }
+
     @Override
     public ScheduledReporter build(MetricRegistry metricRegistry) {
         MetricAdapterFactory metricAdapterFactory = new MetricAdapterFactoryImpl(
                 new DefaultMetricDescriptorLookup(),
                 new DefaultMetricNameTranslator(getReplacements()));
 
+        MonitorableRegistry monitorableRegistry = MonitorableRegistry.getNamedInstance(getRegistryName());
+
+        PcpMmvWriter pcpMmvWriter = new PcpMmvWriter(getPcpName(), IdentifierSourceSet.DEFAULT_SET);
+        pcpMmvWriter.setClusterIdentifier(getClusterIdentifier());
+
+        MonitoringView monitoringView = new PcpMonitorBridge(pcpMmvWriter);
+
+        DynamicMonitoringView dynamicMonitoringView = new DynamicMonitoringView(monitorableRegistry, monitoringView, quietPeriod);
+
         return new ParfaitReporter(metricRegistry,
-                MonitorableRegistry.getNamedInstance(getRegistryName()),
+                monitorableRegistry,
+                dynamicMonitoringView,
                 metricAdapterFactory,
                 getRateUnit(),
                 getDurationUnit(),
