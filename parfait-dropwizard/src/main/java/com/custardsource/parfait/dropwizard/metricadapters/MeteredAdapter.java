5537eccb5b75602e2faf5389d3f6844a54f908a6
Paul Smith <psmith@aconex.com>
2016-01-27 08:40:14 +1100

    Issue #20 MeteredAdaptor now exports a compatible monotonically increasing PCP counter which is generally more useful

--- parfait-dropwizard/src/main/java/com/custardsource/parfait/dropwizard/metricadapters/MeteredAdapter.java
+++ parfait-dropwizard/src/main/java/com/custardsource/parfait/dropwizard/metricadapters/MeteredAdapter.java
@@ -1,16 +1,16 @@
 package com.custardsource.parfait.dropwizard.metricadapters;
 
-import static com.codahale.metrics.MetricRegistry.name;
-
-import javax.measure.unit.Unit;
-import java.util.Set;
-
 import com.codahale.metrics.Metered;
 import com.custardsource.parfait.Monitorable;
 import com.custardsource.parfait.ValueSemantics;
 import com.custardsource.parfait.dropwizard.MetricAdapter;
 import com.custardsource.parfait.dropwizard.NonSelfRegisteringSettableValue;
 import com.google.common.collect.Sets;
+
+import javax.measure.unit.Unit;
+import java.util.Set;
+
+import static com.codahale.metrics.MetricRegistry.name;
 
 public class MeteredAdapter implements MetricAdapter {
 
@@ -19,6 +19,7 @@ public class MeteredAdapter implements MetricAdapter {
     private final NonSelfRegisteringSettableValue<Double> oneMinuteRate;
     private final NonSelfRegisteringSettableValue<Double> meanRate;
     private final NonSelfRegisteringSettableValue<Double> fifteenMinuteRate;
+    private final NonSelfRegisteringSettableValue<Long> rawCounter;
 
     public MeteredAdapter(Metered metered, String name, String description) {
         this.metered = metered;
@@ -26,11 +27,12 @@ public class MeteredAdapter implements MetricAdapter {
         this.fiveMinuteRate = new NonSelfRegisteringSettableValue<>(name(name, "five_minute_rate"), description + " - Five minute rate", Unit.ONE, metered.getFiveMinuteRate(), ValueSemantics.FREE_RUNNING);
         this.oneMinuteRate = new NonSelfRegisteringSettableValue<>(name(name, "one_minute_rate"), description + " - One minute rate", Unit.ONE, metered.getOneMinuteRate(), ValueSemantics.FREE_RUNNING);
         this.meanRate = new NonSelfRegisteringSettableValue<>(name(name, "mean_rate"), description + " - Mean rate", Unit.ONE, metered.getMeanRate(), ValueSemantics.FREE_RUNNING);
+        this.rawCounter = new NonSelfRegisteringSettableValue<>(name, description, Unit.ONE, metered.getCount(),ValueSemantics.MONOTONICALLY_INCREASING);
     }
 
     @Override
     public Set<Monitorable> getMonitorables() {
-        return Sets.<Monitorable>newHashSet(fifteenMinuteRate, fiveMinuteRate, oneMinuteRate, meanRate);
+        return Sets.<Monitorable>newHashSet(fifteenMinuteRate, fiveMinuteRate, oneMinuteRate, meanRate, rawCounter);
     }
 
     @Override
@@ -39,5 +41,6 @@ public class MeteredAdapter implements MetricAdapter {
         fiveMinuteRate.set(metered.getFiveMinuteRate());
         oneMinuteRate.set(metered.getOneMinuteRate());
         meanRate.set(metered.getMeanRate());
+        rawCounter.set(metered.getCount());
     }
 }
--- parfait-dropwizard/src/test/java/com/custardsource/parfait/dropwizard/metricadapters/MeteredAdapterTest.java
+++ parfait-dropwizard/src/test/java/com/custardsource/parfait/dropwizard/metricadapters/MeteredAdapterTest.java
@@ -1,15 +1,11 @@
 package com.custardsource.parfait.dropwizard.metricadapters;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-
-import java.util.Map;
-
-import com.custardsource.parfait.dropwizard.MetricAdapter;
 import com.codahale.metrics.Metered;
 import com.custardsource.parfait.Monitorable;
+import com.custardsource.parfait.ValueSemantics;
+import com.custardsource.parfait.dropwizard.MetricAdapter;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -17,6 +13,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MeteredAdapterTest {
@@ -28,6 +34,7 @@ public class MeteredAdapterTest {
     private static final double INITIAL_FIVE_MINUTE_RATE = 2.2;
     private static final double INITIAL_ONE_MINUTE_RATE = 3.3;
     private static final double INITIAL_MEAN_RATE = 4.4;
+    private static final long INITIAL_RAW_COUNTER_VALUE = 42;
     private static final String FIFTEEN_MINUTE_RATE = "fifteen_minute_rate";
     private static final String FIVE_MINUTE_RATE = "five_minute_rate";
     private static final String ONE_MINUTE_RATE = "one_minute_rate";
@@ -44,6 +51,7 @@ public class MeteredAdapterTest {
         when(metered.getFiveMinuteRate()).thenReturn(INITIAL_FIVE_MINUTE_RATE);
         when(metered.getOneMinuteRate()).thenReturn(INITIAL_ONE_MINUTE_RATE);
         when(metered.getMeanRate()).thenReturn(INITIAL_MEAN_RATE);
+        when(metered.getCount()).thenReturn(INITIAL_RAW_COUNTER_VALUE);
 
         meteredAdapter = new MeteredAdapter(metered, NAME, DESCRIPTION);
     }
@@ -74,6 +82,41 @@ public class MeteredAdapterTest {
         assertThat(extractMonitorables(meteredAdapter).get(MEAN_RATE), notNullValue());
         assertThat(extractMonitorables(meteredAdapter).get(MEAN_RATE).getDescription(), is(DESCRIPTION + " - Mean rate"));
         assertThat(extractMonitorables(meteredAdapter).get(MEAN_RATE).get(), Matchers.<Object>is(INITIAL_MEAN_RATE));
+    }
+
+    @Test
+    public void shouldPublishRawCounterMetric() {
+        Monitorable baseRawCounter = locateRawCounter();
+        assertNotNull("There should have been a Monitorable with the base name", baseRawCounter);
+
+        assertEquals("The base raw counter should be a PCP counter (Monotonically Increasing)", baseRawCounter.getSemantics(), ValueSemantics.MONOTONICALLY_INCREASING);
+
+        assertThat(baseRawCounter.get(), Matchers.<Object>is(INITIAL_RAW_COUNTER_VALUE));
+    }
+
+    private Monitorable locateRawCounter() {
+        Predicate<Monitorable> baseRawCounterPredicate = new Predicate<Monitorable>() {
+            @Override
+            public boolean apply(Monitorable monitorable) {
+                return monitorable.getName().equals(NAME);
+            }
+        };
+
+        assertTrue("There should have been a Monitorable with the base name", Iterables.any(meteredAdapter.getMonitorables(), baseRawCounterPredicate));
+        return Iterables.find(meteredAdapter.getMonitorables(), baseRawCounterPredicate);
+    }
+
+    @Test
+    public void shouldUpdateValues() {
+        when(metered.getMeanRate()).thenReturn(43.0);
+        when(metered.getCount()).thenReturn(43L);
+
+        Monitorable rawCounter = locateRawCounter();
+
+        meteredAdapter.updateMonitorables();
+
+        assertThat("The Mean value has not been updated since the initial value was set", extractMonitorables(meteredAdapter).get(MEAN_RATE).get(), Matchers.<Object>is(43.0));
+        assertThat("The raw counter value has not been updated since the initial value was set", rawCounter.get(), Matchers.<Object>is(43L));
     }
 
     private Map<String, Monitorable> extractMonitorables(MetricAdapter timerAdapter) {
