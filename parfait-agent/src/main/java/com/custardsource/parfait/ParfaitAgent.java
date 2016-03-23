ee72b67a16849ea1d492a207f1da14f1f82d1a49
Nathan Scott <nathans@redhat.com>
2016-03-23 15:49:34 +1100

    parfait-agent: functional enhancements, JMX metrics working too
    Deal with the absence of spring-utils schema by hand-coding the
    list of JVM metrics into a javax.util.list object.  This is now
    found by Spring and all the JMX metrics are auto-wired into the
    registry.
    
    Remove several no-longer-used java imports - Spring IoC handles
    all of that now.
    
    Use TallPauls Spring PropertyPlaceholderConfigurer tip, and now
    the jvm.version metric is working as nature intended.  Started
    using System properties more widely throughout as a result, and
    several parameters are pushed into the properties file now.
    
    Discovered the G1 garbage collector metrics needed.  In Java 8,
    the perm gen is completely removed (so these metrics commented
    out for now) - we'll need to build a way to only optionally
    instantiate some beans, depending on JVM version.  I see Aconex
    folk have done before using an optional metric registry so that
    might be the best way to go here too - OptionalFactoryBean from
    http://stackoverflow.com/questions/3957430/optional-spring-bean-references
    describes that approach.

--- parfait-agent/src/main/java/com/custardsource/parfait/ParfaitAgent.java
+++ parfait-agent/src/main/java/com/custardsource/parfait/ParfaitAgent.java
@@ -1,26 +1,15 @@
 package com.custardsource.parfait;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.measure.unit.Unit;
-
 import com.custardsource.parfait.DynamicMonitoringView;
 import com.custardsource.parfait.MonitorableRegistry;
 import com.custardsource.parfait.pcp.PcpMonitorBridge;
-import com.custardsource.parfait.dxm.IdentifierSourceSet;
-import com.custardsource.parfait.dxm.PcpMmvWriter;
-import com.custardsource.parfait.dxm.PcpMmvWriter.MmvFlag;
-import com.custardsource.parfait.dxm.MetricName;
-import com.custardsource.parfait.dxm.semantics.Semantics;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -73,6 +62,10 @@ public class ParfaitAgent {
         } else {
             name = defaultName(runtimeName);
         }
+        System.setProperty("parfait.name", name);
+        // TODO: properties hacks to get things bootstrapped (cleanup!)
+        System.setProperty("parfait.cluster", "42");
+        System.setProperty("parfait.interval", "1000");
 
         logger.info("Starting Parfait agent {} with arguments {}", name, args);
 
@@ -81,19 +74,20 @@ public class ParfaitAgent {
             ApplicationContext context = new ClassPathXmlApplicationContext("monitoring.xml");
             MonitorableRegistry metrics = (MonitorableRegistry)context.getBean("monitorableRegistry");
             PcpMonitorBridge bridge = (PcpMonitorBridge)context.getBean("pcpMonitorBridge");
-/
+
+//          TODO: TallPaul has the Spring magic to do this correctly:
 //          PcpMmvWriter writer = (PcpMmvWriter)context.getBean("mmvPcpWriter");
 //          // keep the prefix (remove noprefix flag) and monitor the PID
 //          writer.setFlags(EnumSet.of(MmvFlag.MMV_FLAG_PROCESS));
+
 //          // TODO: addShutdownHook - cleanup mmapped file on orderly shutdown
-//          // (extend PcpMmvWriter? - it has the path to unlink)
+//          // (extend PcpMmvWriter?  it keeps the path we wish to unlink here)
 
             DynamicMonitoringView view = new DynamicMonitoringView(metrics, bridge);
             view.start();
 
-        } catch (BeansException beansError) {
-            logger.error("Stopping Parfait agent, cannot setup beans");
-            logger.debug(beansError.getMessage());
+        } catch (BeansException e) {
+            logger.error("Stopping Parfait agent, cannot setup beans", e);
         }
     }
 }
