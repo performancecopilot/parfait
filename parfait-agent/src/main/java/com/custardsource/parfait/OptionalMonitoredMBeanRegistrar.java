cf33b58884504178dcf6560556343809732786e0
Nathan Scott <nathans@redhat.com>
2016-04-06 16:46:02 +1000

    Allow optional managed bean definitions, add metaspace metrics
    The permanent generation metrics for the various garbage collection
    implementations have had problems related to whether or not metrics
    exist within a JVM.  Original parfait handles the case where permgen
    metrics is one of a set of named beans, but not the case from Java 8
    where permgen is done away with altogether (replaced with metaspace,
    and the alternate G1 algorithms).
    
    This was a problem originally, as we attempted to define spring beans
    that didn't exist.  We now introduce a mechanism for optional metrics
    and use this to (re)introduce permgen and metaspace metrics.

---
+++ parfait-agent/src/main/java/com/custardsource/parfait/OptionalMonitoredMBeanRegistrar.java
@@ -0,0 +1,28 @@
+package com.custardsource.parfait;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.IntrospectionException;
+import javax.management.ReflectionException;
+import javax.management.MBeanException;
+
+import com.custardsource.parfait.jmx.MonitoredMBeanAttributeFactory;
+
+public class OptionalMonitoredMBeanRegistrar<T> extends MonitoredMBeanAttributeFactory<T> {
+
+    public OptionalMonitoredMBeanRegistrar(String name, String description,
+            String mBeanName, String attributeName, String compositeDataItem) {
+        super(name, description, mBeanName, attributeName, compositeDataItem);
+    }
+
+    public Monitorable<T> getObject() throws InstanceNotFoundException, IntrospectionException,
+            ReflectionException, AttributeNotFoundException, MBeanException {
+        try {
+            return super.getObject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
