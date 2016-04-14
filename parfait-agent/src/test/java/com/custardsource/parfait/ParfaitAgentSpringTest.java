895634314a78c56ed8bca1faf7af179615573f13
Nathan Scott <nathans@redhat.com>
2016-04-14 16:03:04 +1000

    Begin using the spring-test framework in ParfaitAgent
    This required a bump to the Junit version beyond 4.9 - went with
    current latest which is 4.12 for now.
    
    Agent test added which verifies that all the spring bean loading
    activity which -javaagent mode would invoke happens without any
    errors, and the DynamicMonitoringView is successfully created.
    
    The test uses a custom properties file for injecting the needed
    parfait-agent-specific properties, which meant augmenting the bean
    configuration with an additional file to load in custom properties
    - hence the @ContextConfiguration test class annotation lists both
    of those files.

---
+++ parfait-agent/src/test/java/com/custardsource/parfait/ParfaitAgentSpringTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.custardsource.parfait.DynamicMonitoringView;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:test.xml", "classpath:java.xml" })
+public class ParfaitAgentSpringTest {
+
+    @Autowired
+    private DynamicMonitoringView monitoringView;
+
+    @Test
+    public void testParfaitAgentSpringBeanWiring() {
+        assertEquals("class com.custardsource.parfait.DynamicMonitoringView", this.monitoringView.getClass().toString());
+    }
+}
