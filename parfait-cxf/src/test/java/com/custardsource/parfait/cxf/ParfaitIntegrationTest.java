ac6d15595c23225c45e222b2ad52c5156cd7bb62
Nathan Scott <nathans@redhat.com>
2016-04-14 15:56:18 +1000

    Shuffle forward to using current spring version (4.2.5)
    We need a newer Spring for incoming parfait-agent Spring testing.
    Updates needed to one of the CXF tests, where the new version was
    seeing a FileNotFound exception - traced back to use of port 9080
    for the test-created http server, which generates an exception if
    one reads from the output stream rather than error stream.

--- parfait-cxf/src/test/java/com/custardsource/parfait/cxf/ParfaitIntegrationTest.java
+++ parfait-cxf/src/test/java/com/custardsource/parfait/cxf/ParfaitIntegrationTest.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.HttpURLConnection;
 
 import org.apache.cxf.testutil.common.AbstractTestServerBase;
 import org.junit.After;
@@ -16,29 +17,38 @@ import org.junit.Test;
 public class ParfaitIntegrationTest {
     private AbstractTestServerBase testServer;
 
-	@Before
+    @Before
     public void startServer() throws Exception {
-    	testServer = new SpringCreatedTestServer();
-    	testServer.startInProcess();
+        testServer = new SpringCreatedTestServer();
+        testServer.startInProcess();
     }
 
-	@After
+    @After
     public void stopServer() throws Exception {
-    	testServer.tearDown();
+        testServer.tearDown();
     }
 
     @Test
     public void testInterceptor() throws Exception {
-    	hitURL("restdemo/sayhello");
-    	hitURL("restdemo/snooze");
+        hitURL("restdemo/sayhello");
+        hitURL("restdemo/snooze");
     }
 
-	private void hitURL(String fragment) throws MalformedURLException, IOException {
-		String endpointAddress = "http://localhost:9080/api/" + fragment;
+    private void hitURL(String fragment) throws MalformedURLException, FileNotFoundException, IOException {
+        String endpointAddress = "http://localhost:9080/api/" + fragment;
         URL url = new URL(endpointAddress);
-        URLConnection connect = url.openConnection();
-        InputStream in = connect.getInputStream();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setReadTimeout(2000);    // give it up to 2 seconds to respond
+        connection.connect();
+
+        // Non-standard ports (9080) can result in an exception from connect.getInputStream() below;
+        // in HTTP error cases, HttpURLConnection gives back the input stream via #getErrorStream().
+        // 4xx: client error, 5xx: server error. http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html.
+
+        boolean isError = connection.getResponseCode() >= 400;
+        InputStream in = isError ? connection.getErrorStream() : connection.getInputStream();
         assertNotNull(in);
-	}
+    }
 
 }
