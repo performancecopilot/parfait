491e58bb97fd04cb84174664997d529e854ed648
Nathan Scott <nathans@redhat.com>
2016-04-29 11:02:04 +1000

    Remove old scripts, sample/demo program for parfait-agent

--- parfait-agent/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh -x
-mvn clean package install assembly:single
--- parfait-agent/demo/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-[ -f AgentDemo.java ] || javac AgentDemo.java
-
-agent=../target/parfait-agent-jar-with-dependencies.jar
-java -javaagent:$agent AgentDemo
