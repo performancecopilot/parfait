966bfd31d9ad5c05a26b9b8e4d0a2f19fc452d00
Nathan Scott <nathans@redhat.com>
2016-05-24 12:55:09 +1000

    Fix partfait-spring resource leak on application XML class path load.

--- parfait-spring/src/test/java/io/pcp/parfait/spring/MonitorTest.java
+++ parfait-spring/src/test/java/io/pcp/parfait/spring/MonitorTest.java
@@ -5,7 +5,6 @@ import io.pcp.parfait.MonitorableRegistry;
 import io.pcp.parfait.MonitoredCounter;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterators;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import junit.framework.TestCase;
@@ -14,36 +13,38 @@ import java.util.Collection;
 
 
 public class MonitorTest extends TestCase {
-	public void testThing() {
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"test.xml"});
-		DelayingBean test = ((DelayingBean) context.getBean("test"));
-		DelayingBean other = ((DelayingBean) context.getBean("other"));
+    public void testThing() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"test.xml"});
+        try {
+            DelayingBean test = ((DelayingBean) context.getBean("test"));
+            DelayingBean other = ((DelayingBean) context.getBean("other"));
 
-        MonitorableRegistry registry = (MonitorableRegistry) context.getBean("registry");
+            MonitorableRegistry registry = (MonitorableRegistry) context.getBean("registry");
 
-        assertNotNull("registry should not be null", registry);
+            assertNotNull("registry should not be null", registry);
 
-        assertTrue("should have located counter", counterExists(registry, "xx.other.count"));
-        assertTrue("should have located counter", counterExists(registry, "xx.test.count"));
+            assertTrue("should have located counter", counterExists(registry, "xx.other.count"));
+            assertTrue("should have located counter", counterExists(registry, "xx.test.count"));
 
-        MonitoredCounter otherCounter = (MonitoredCounter) findCounter(registry, "xx.other.count");
-        MonitoredCounter testCounter = (MonitoredCounter) findCounter(registry, "xx.test.count");
+            MonitoredCounter otherCounter = (MonitoredCounter) findCounter(registry, "xx.other.count");
+            MonitoredCounter testCounter = (MonitoredCounter) findCounter(registry, "xx.test.count");
 
-        assertNotNull("should have located the basic counter for 'other'", otherCounter);
-        assertEquals(0L, otherCounter.get().longValue());
+            assertNotNull("should have located the basic counter for 'other'", otherCounter);
+            assertEquals(0L, otherCounter.get().longValue());
 
-        assertNotNull("should have located the basic counter for 'test'", testCounter);
-        assertEquals(0L, testCounter.get().longValue());
-
-
-		test.doThing();
-		other.doThing();
-
-        assertEquals(1L, otherCounter.get().longValue());
-        assertEquals(1L, testCounter.get().longValue());
+            assertNotNull("should have located the basic counter for 'test'", testCounter);
+            assertEquals(0L, testCounter.get().longValue());
 
 
-	}
+            test.doThing();
+            other.doThing();
+
+            assertEquals(1L, otherCounter.get().longValue());
+            assertEquals(1L, testCounter.get().longValue());
+        } finally {
+            context.close();
+        }
+    }
 
     // TODO some Hamcrest matcher goodness on assertions that specific monitorable exists?
     private Monitorable<?> findCounter(MonitorableRegistry registry, final String name) {
@@ -59,7 +60,6 @@ public class MonitorTest extends TestCase {
             throw new IllegalArgumentException(name + " could not be located in the MonitorableRegistry");
         }
         return monitorable;
-
     }
 
     private boolean counterExists(MonitorableRegistry registry, String name) {
@@ -70,6 +70,4 @@ public class MonitorTest extends TestCase {
         }
         return false;
     }
-
-
 }
