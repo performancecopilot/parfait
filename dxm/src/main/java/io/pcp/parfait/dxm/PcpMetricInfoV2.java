444230da8bc55d8695cc5614fa7f82a89490a442
Ryan Doyle <ryan@doylenet.net>
2016-10-30 12:25:12 +1100

    MMVv2: Actually implement PcpMetricInfoV2
    The test isn't the /best/ but I think its better than a bunch of verifications
    on a ByteBuffer mock. It also gives a clear picture of the expected format.

--- dxm/src/main/java/io/pcp/parfait/dxm/PcpMetricInfoV2.java
+++ dxm/src/main/java/io/pcp/parfait/dxm/PcpMetricInfoV2.java
@@ -1,11 +1,13 @@
 package io.pcp.parfait.dxm;
 
 import io.pcp.parfait.dxm.PcpMmvWriter.Store;
+import io.pcp.parfait.dxm.semantics.UnitMapping;
 
 import java.nio.ByteBuffer;
 import java.util.Set;
 
 class PcpMetricInfoV2 extends PcpMetricInfo {
+    private static final int METRIC_LENGTH = 48;
     private PcpString nameAsPcpString;
 
     PcpMetricInfoV2(String metricName, int id, PcpString nameAsPcpString) {
@@ -15,12 +17,27 @@ class PcpMetricInfoV2 extends PcpMetricInfo {
 
     @Override
     public void writeToMmv(ByteBuffer byteBuffer) {
-        throw new RuntimeException("Not implemented");
+        byteBuffer.position(offset);
+
+        byteBuffer.putLong(getStringOffset(nameAsPcpString));
+        byteBuffer.putInt(getId());
+        byteBuffer.putInt(typeHandler.getMetricType().getIdentifier());
+        byteBuffer.putInt(getSemantics().getPcpValue());
+        byteBuffer.putInt(UnitMapping.getDimensions(getUnit(), metricName));
+        if (domain != null) {
+            byteBuffer.putInt(domain.getId());
+        } else {
+            byteBuffer.putInt(DEFAULT_INSTANCE_DOMAIN_ID);
+        }
+        // Just padding
+        byteBuffer.putInt(0);
+        byteBuffer.putLong(getStringOffset(shortHelpText));
+        byteBuffer.putLong(getStringOffset(longHelpText));
     }
 
     @Override
     public int byteSize() {
-        throw new RuntimeException("Not implemented");
+        return METRIC_LENGTH;
     }
 
     static final class MetricInfoStoreV2 extends Store<PcpMetricInfo> {
--- dxm/src/test/java/io/pcp/parfait/dxm/PcpMetricInfoV2Test.java
+++ dxm/src/test/java/io/pcp/parfait/dxm/PcpMetricInfoV2Test.java
@@ -1,12 +1,22 @@
 package io.pcp.parfait.dxm;
 
 import io.pcp.parfait.dxm.PcpMetricInfoV2.MetricInfoStoreV2;
+import io.pcp.parfait.dxm.semantics.Semantics;
+import io.pcp.parfait.dxm.types.MmvMetricType;
+import io.pcp.parfait.dxm.types.TypeHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.nio.ByteBuffer;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,21 +24,81 @@ import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEqua
 
 public class PcpMetricInfoV2Test {
 
+    private static final int STRING_OFFSET = 10;
+    private static final int METRIC_TYPE_IDENTIFIER = 909;
+    private static final int SHORT_HELP_TEXT_OFFSET = 11;
+    private static final int LONG_HELP_TEXT_OFFSET = 12;
+    private static final int EXPECTED_PCP_METRIC_INFO_SIZE = 48;
+    private static final int SEMANTICS_VALUE = 32;
+    private static final int THIS_PCP_METRIC_INFO_OFFSET = 400;
+
     private PcpMetricInfoV2 pcpMetricInfoV2;
+
+    @Mock
+    private PcpString nameAsPcpString;
 
     @Before
     public void setUp() throws Exception {
-        pcpMetricInfoV2 = new PcpMetricInfoV2("name", 1, mock(PcpString.class));
+        MockitoAnnotations.initMocks(this);
+        pcpMetricInfoV2 = new PcpMetricInfoV2("name", 1, nameAsPcpString);
     }
 
-    @Test(expected = RuntimeException.class)
-    public void writeToMmvShouldRaiseAnErrorAsItIsNotImplementedYet() {
-        pcpMetricInfoV2.writeToMmv(mock(ByteBuffer.class));
+    @Test
+    public void writeToMmvWriteToTheByteBuffer() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+        TypeHandler<Object> typeHandler = mock(TypeHandler.class);
+        MmvMetricType mmvMetricType = mock(MmvMetricType.class);
+        PcpString shortHelpPcpString = mock(PcpString.class);
+        PcpString longHelpPcpString = mock(PcpString.class);
+        Semantics semantics = mock(Semantics.class);
+
+
+        when(nameAsPcpString.getOffset()).thenReturn(STRING_OFFSET);
+        when(typeHandler.getMetricType()).thenReturn(mmvMetricType);
+        when(mmvMetricType.getIdentifier()).thenReturn(METRIC_TYPE_IDENTIFIER);
+        when(shortHelpPcpString.getOffset()).thenReturn(SHORT_HELP_TEXT_OFFSET);
+        when(longHelpPcpString.getOffset()).thenReturn(LONG_HELP_TEXT_OFFSET);
+        when(semantics.getPcpValue()).thenReturn(SEMANTICS_VALUE);
+
+        pcpMetricInfoV2.setTypeHandler(typeHandler);
+        pcpMetricInfoV2.setHelpText(shortHelpPcpString, longHelpPcpString);
+        pcpMetricInfoV2.setSemantics(semantics);
+
+        pcpMetricInfoV2.writeToMmv(byteBuffer);
+
+        byte[] expectedBytes = {
+            0,  0,  0,  0,  0,  0,  0,  10,
+            0,  0,  0,  1,  0,  0,  3,  -115,
+            0,  0,  0,  32,  0,  0,  0,  0,
+            -1, -1, -1, -1, 0,  0,  0,  0,
+            0,  0,  0,  0,  0,  0,  0,  11,
+            0,  0,  0,  0,  0,  0,  0,  12,
+        };
+
+        assertArrayEquals(expectedBytes, byteBuffer.array());
     }
 
-    @Test(expected = RuntimeException.class)
-    public void byteSizeShouldRaiseAnErrorAsItIsNotImplementedYet() {
-        pcpMetricInfoV2.byteSize();
+    @Test
+    public void writeToMmvShouldSetThePositionOfTheBufferBeforeWriting() {
+        ByteBuffer byteBuffer = mock(ByteBuffer.class);
+        TypeHandler<Object> typeHandler = mock(TypeHandler.class);
+
+        when(typeHandler.getMetricType()).thenReturn(mock(MmvMetricType.class));
+
+        pcpMetricInfoV2.setOffset(THIS_PCP_METRIC_INFO_OFFSET);
+        pcpMetricInfoV2.setTypeHandler(typeHandler);
+
+        pcpMetricInfoV2.writeToMmv(byteBuffer);
+
+        InOrder inOrder = Mockito.inOrder(byteBuffer);
+
+        inOrder.verify(byteBuffer).position(THIS_PCP_METRIC_INFO_OFFSET);
+        inOrder.verify(byteBuffer).putLong(anyLong());
+    }
+
+    @Test
+    public void byteSizeShouldReturnTheByteSizeOfTheMmvFormat() {
+        assertEquals(EXPECTED_PCP_METRIC_INFO_SIZE, pcpMetricInfoV2.byteSize());
     }
 
     @Test
