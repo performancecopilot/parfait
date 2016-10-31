02775f319e3f7ef2b985e0a9411bd5da7fd69ca7
Ryan Doyle <ryan@doylenet.net>
2016-10-31 15:20:22 +1100

    MMVv2: Fix broken test
    Somehow I missed this before. Looks like it was committed broken.

--- dxm/src/test/java/io/pcp/parfait/dxm/InstanceDomainTest.java
+++ dxm/src/test/java/io/pcp/parfait/dxm/InstanceDomainTest.java
@@ -8,6 +8,8 @@ import java.util.Collection;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -18,12 +20,13 @@ public class InstanceDomainTest {
     @Test
     public void shouldReturnInstancesAssociatedWithTheStore() {
         InstanceStoreFactory instanceStoreFactory = mock(InstanceStoreFactory.class);
-        InstanceDomain instanceDomain = new InstanceDomain(DOMAIN, 111, instanceStoreFactory);
         Store<Instance> instanceStore = mock(Store.class);
         Collection<Instance> instances = singletonList(mock(Instance.class));
 
-        when(instanceStoreFactory.createNewInstanceStore(DOMAIN, instanceDomain)).thenReturn(instanceStore);
+        when(instanceStoreFactory.createNewInstanceStore(eq(DOMAIN), any(InstanceDomain.class))).thenReturn(instanceStore);
         when(instanceStore.all()).thenReturn(instances);
+
+        InstanceDomain instanceDomain = new InstanceDomain(DOMAIN, 111, instanceStoreFactory);
 
         assertThat(instanceDomain.getInstances(), is(instances));
     }
