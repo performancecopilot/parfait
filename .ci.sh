7e563ed732b48d69f1731f05422c12ef5b3bcc9a
psmith <psmith@aconex.com>
2020-09-09 17:28:15 +1000

    Issue #69 Attempt to try 'focal' Ubuntu distribution for testing against latest PCP

--- .ci.sh
+++ .ci.sh
@@ -8,8 +8,8 @@ apt-get install -y apt-transport-https
 # Setup PCP
 # Due to Ubuntu/Python dependency hell, building against the latest PCP from bintray just doesn't install
 # TODO need to fix this - see Issue #69
-#wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=pcp | apt-key add -
-#echo "deb https://dl.bintray.com/pcp/bionic bionic main" >> /etc/apt/sources.list
+wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=pcp | apt-key add -
+echo "deb https://dl.bintray.com/pcp/focal focal main" >> /etc/apt/sources.list
 apt-get -qq update
 #apt-get install -y python3.6
 apt-get install -y pcp pcp-gui
